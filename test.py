681c36a2bf3a4284bd93673ef044676a45b071b6
anshikaasinghh <145269701+anshikaasinghh@users.noreply.github.com>
2025-06-14 22:24:06 +0530

    modified test.py for demo hosting

--- test.py
+++ test.py
@@ -1,6 +1,11 @@
 import streamlit as st
 import google.generativeai as genai
 
+
+import os
+os.system("pip install google-generativeai==0.8.3")
+
+
 st.title("Test Google Generative AI")
 genai.configure(api_key="AIzaSyAUD7wyBizj2rH_UO5N8MuMIgz7-9_1tfk")
 model = genai.GenerativeModel("gemini-1.5-flash")
