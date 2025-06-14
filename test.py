62b0c8a4da114289ac5a47e1468f3ae69af8ce44
anshikaasinghh <145269701+anshikaasinghh@users.noreply.github.com>
2025-06-14 22:20:05 +0530

    added tes.py for demo hosting

---
+++ test.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import google.generativeai as genai
+
+st.title("Test Google Generative AI")
+genai.configure(api_key="AIzaSyAUD7wyBizj2rH_UO5N8MuMIgz7-9_1tfk")
+model = genai.GenerativeModel("gemini-1.5-flash")
+st.write("Model loaded successfully!")
