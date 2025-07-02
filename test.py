f9d4921ba6f831c1e558c3a0e60491ed59f37984
anshikaasinghh <145269701+anshikaasinghh@users.noreply.github.com>
2025-07-02 16:45:27 +0530

    Update test.py

--- test.py
+++ test.py
@@ -9,6 +9,6 @@ subprocess.run(["pip", "install", "--upgrade", "google-generativeai"])
 
 
 st.title("Test Google Generative AI")
-genai.configure(api_key="AIzaSyAUD7wyBizj2rH_UO5N8MuMIgz7-9_1tfk")
+genai.configure(api_key="xxx")
 model = genai.GenerativeModel("gemini-1.5-flash")
 st.write("Model loaded successfully!")
